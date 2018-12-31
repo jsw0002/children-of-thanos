f7c175bc23cad837a7ce2a7c01ce2a80052ca563
Jordan Willis <jsw0002@gmail.com>
2018-12-31 18:00:10 -0500

    added the rest of the sections without content

--- components/Constitution/ConstitutionTabs.vue
+++ components/Constitution/ConstitutionTabs.vue
@@ -9,14 +9,19 @@
           <league-financing></league-financing>
         </b-tab>
         <b-tab title="League Setup & Compeition">
+          <league-setup-competition></league-setup-competition>
         </b-tab>
         <b-tab title="Rosters & Lineups">
+          <rosters-and-lineups></rosters-and-lineups>
         </b-tab>
         <b-tab title="Scoring System">
+          <scoring-system></scoring-system>
         </b-tab>
         <b-tab title="Offseason Activities">
+          <offseason-activities></offseason-activities>
         </b-tab>
         <b-tab title="Amendments">
+          <amendments></amendments>
         </b-tab>
       </b-tabs>
     </b-card>
@@ -26,11 +31,21 @@
 <script>
 import LeagueOverview from '~/components/Constitution/LeagueOverview';
 import LeagueFinancing from '~/components/Constitution/LeagueFinancing';
+import LeagueSetupCompetition from '~/components/Constitution/LeagueSetupCompetition';
+import RostersAndLineups from '~/components/Constitution/RostersAndLineups';
+import ScoringSystem from '~/components/Constitution/ScoringSystem';
+import OffseasonActivities from '~/components/Constitution/OffseasonActivities';
+import Amendments from '~/components/Constitution/Amendments';
 
 export default {
   components: {
     LeagueOverview,
     LeagueFinancing,
+    LeagueSetupCompetition,
+    RostersAndLineups,
+    ScoringSystem,
+    OffseasonActivities,
+    Amendments,
   },
 };
 </script>
---
+++ components/Constitution/ScoringSystem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+2
+  </div>
+</template>
+
+<script>
+import SubSection from '~/components/SubSection';
+
+export default {
+  components: {
+    SubSection,
+  },
+};
+</script>
+
+<style>
+
+</style>
