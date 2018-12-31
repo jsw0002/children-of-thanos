7d902b6e206468cf72e7f933b62f13d12a007b48
Jordan Willis <jsw0002@gmail.com>
2018-12-31 11:06:32 -0500

    added constitution section

---
+++ components/Constitution/Constitution.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <title-section>Constitution</title-section>
+    <constitution-tabs></constitution-tabs>
+  </section>
+</template>
+
+<script>
+import TitleSection from '~/components/TitleSection';
+import ConstitutionTabs from '~/components/Constitution/ConstitutionTabs';
+
+export default {
+  components: {
+    TitleSection,
+    ConstitutionTabs,
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ components/Constitution/ConstitutionTabs.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="row">
+    <b-card no-body>
+      <b-tabs pills card vertical>
+        <b-tab title="League Overview" active>
+          <league-overview></league-overview>
+        </b-tab>
+        <b-tab title="League Financing">
+        </b-tab>
+        <b-tab title="League Setup & Compeition">
+        </b-tab>
+        <b-tab title="Rosters & Lineups">
+        </b-tab>
+        <b-tab title="Scoring System">
+        </b-tab>
+        <b-tab title="Offseason Activities">
+        </b-tab>
+        <b-tab title="Amendments">
+        </b-tab>
+      </b-tabs>
+    </b-card>
+  </div>
+</template>
+
+<script>
+import LeagueOverview from '~/components/Constitution/LeagueOverview';
+
+export default {
+  components: {
+    LeagueOverview,
+  }
+}
+</script>
+
+<style>
+
+</style>
